606a3237be617499bb4ea261372892090889581b
Jeniel Pusa <jeniel12300@gmail.com>
2025-08-22 17:46:57 +0800

    Deploy in hostinger 2

--- Back-End/app.js
+++ Back-End/app.js
@@ -49,6 +49,15 @@ const logger = function (req, res, next) {
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.set("trust proxy", true);
+
+
+// CHECK environment variables
+console.log("CONN_STR:", process.env.CONN_STR);
+console.log("NODE_ENV:", process.env.NODE_ENV);
+console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
+
+
+console.log("Setting up MongoStore with URL:", process.env.CONN_STR);
 app.use(
   session({
     secret: process.env.SECRET_STR,
